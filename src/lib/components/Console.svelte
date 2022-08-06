b24db785383d9da9b0065b77496dec049ae5d394
nico-mayer <nico.m94@icloud.com>
2022-08-06 21:21:20 +0200

    console improvements

--- src/lib/components/Console.svelte
+++ src/lib/components/Console.svelte
@@ -2,15 +2,44 @@
 	import { consoleContent } from '../stores/console.js';
 	import { consoleOpen } from '../stores/console.js';
 	import { slide } from 'svelte/transition';
+	import { resizing } from '../stores/store.js';
+
+	function handleResize() {
+		let consoleContainer = document.getElementById('consoleContainer');
+		$resizing = true;
+		window.addEventListener('mousemove', handleMouseMove);
+		window.addEventListener('mouseup', handleMouseUp);
+
+		function handleMouseMove(e) {
+			if (
+				window.innerHeight - e.clientY > 100 &&
+				window.innerHeight - e.clientY < window.innerHeight - 100
+			) {
+				consoleContainer.style.height = window.innerHeight - e.clientY + 'px';
+				document.body.style.cursor = 'row-resize';
+			}
+		}
+
+		function handleMouseUp() {
+			$resizing = false;
+			document.body.style.cursor = 'auto';
+			window.removeEventListener('mousemove', handleMouseMove);
+			window.removeEventListener('mouseup', handleMouseUp);
+		}
+	}
 </script>
 
 {#key $consoleOpen}
 	<main
 		class="bg-panelBgLight dark:bg-panelBgDark h-40 flex-col hidden w-full absolute bottom-0"
+		id="consoleContainer"
 		class:flex={$consoleOpen}
 		transition:slide
 	>
-		<div class="px-2 py-3 bg-editorBgLight dark:bg-editorBgDark flex items-center">
+		<div
+			class="px-2 py-3 bg-editorBgLight dark:bg-editorBgDark flex items-center cursor-row-resize"
+			on:mousedown={handleResize}
+		>
 			<div class="i-mdi-console-line mr-1 text-xl" />
 			<span>console:</span>
 		</div>
